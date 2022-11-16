1a9e50fc4eaf665ea97023823e2eef012e7cbadb
Trang Huynh <tranghuynh@c8-89-f3-ed-96-db.wlan.lsu.edu>
2022-11-16 11:01:57 -0600

    initial commit

---
+++ testfs4a.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "filesystem.h"
+#include "softwaredisk.h"
+
+// RUN formatfs before conducting this test then run testfs4b *after* this test
+  
+int main(int argc, char *argv[]) {
+  int ret, i;
+  File f;
+  char buf[SOFTWARE_DISK_BLOCK_SIZE];
+
+  // tests block alignment, along with testfs4b
+  
+  f=create_file("line-em-up");
+  printf("ret from create_file(\"line-em-up\") = %p\n",
+	 f);
+  fs_print_error();
+
+  if (f) {
+    for (i=0; i < 50; i++) {
+      memset(buf, 'A'+i, SOFTWARE_DISK_BLOCK_SIZE);
+      ret = write_file(f, buf, SOFTWARE_DISK_BLOCK_SIZE);
+      printf("ret from write_file(f, buf, SOFTWARE_DISK_BLOCK_SIZE) = %d\n",
+	     ret);
+      fs_print_error();
+    }
+    
+    close_file(f);
+    printf("Executed close_file(f).\n");
+    fs_print_error();
+  }
+  else {
+    printf("FAIL.  Did you run formatfs before this test?\n");
+  }
+  
+  return 0;
+}
